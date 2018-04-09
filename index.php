8b633a6e4480ee10932ff71786dd47b99f2f6cdb
LETAILLEUR ARNAUD <letailleurarnaud@hotmail.fr>
2018-04-09 15:37:29 +0200

    bonjour

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo "<h1>the turtle project</h1>";
+echo "<h1>lol</h1>";
+$message = "<ul>";
+
+for ($i = -17827 ; $i <= 14568 ; $i+= 7){
+    if ($i % 24 == 0) {
+        $message .= "<li>";
+        $message .= $i;
+        $message .= "<li>";
+    }
+}
+
+$message = "<ul>";
+
+echo $message;
